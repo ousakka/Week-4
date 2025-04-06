4ef11bd6b1e4242a46261840fc6d09d2ffff3d43
Tim <13306362+timmarschall@users.noreply.github.com>
2025-04-06 18:36:01 +0200

    style: format everything using prettier

--- homework/script.js
+++ homework/script.js
@@ -1,25 +1,20 @@
 let count = 0;
 
-function display()
-{
-    document.getElementById("count").textContent=count;
+function display() {
+  document.getElementById("count").textContent = count;
 }
 
-function increase()
-{
-    count++;
-    display();
+function increase() {
+  count++;
+  display();
 }
 
-function decrease()
-{
-    count--;
-    display();
+function decrease() {
+  count--;
+  display();
 }
 
-function Reset()
-{
-    count=0;
-    display();
+function Reset() {
+  count = 0;
+  display();
 }
-
